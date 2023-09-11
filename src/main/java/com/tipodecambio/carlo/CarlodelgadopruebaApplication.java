03b94a4883405dc6793257090ff4d8802e5b1a95
carlo delgado <cdmarallano23@gmail.com>
2023-09-10 21:50:03 -0500

    [1] implementacion de proyecto de data cambiomoneda

---
+++ src/main/java/com/tipodecambio/carlo/CarlodelgadopruebaApplication.java
@@ -0,0 +1,13 @@
+package com.tipodecambio.carlo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarlodelgadopruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarlodelgadopruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tipodecambio/carlo/CarlodelgadopruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tipodecambio.carlo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarlodelgadopruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
